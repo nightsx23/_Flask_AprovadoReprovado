c297ee0b255dc5cd20dba1539ecda15b2a91b432
nightsx23 <andrews.fernandesx@gmail.com>
2020-10-11 17:18:23 -0300

    Primeiro commit

---
+++ _flask_Primeira Api/main.py
@@ -0,0 +1,50 @@
+from flask import Flask  # importa a biblioteca flask
+from flask.globals import request
+from flask.json import jsonify
+from flask.templating import render_template
+
+app = Flask(__name__) #inicializa a API
+
+@app.route('/') #cria uma rota
+def main():
+  resultado = None
+  media     = None
+  
+  # Parametros inseridos via template html
+  primeira = request.args.get('primeira')
+  segunda  = request.args.get('segunda')
+  
+  # Parametros inseridos formato JSON (Postman)
+  #primeira = request.json['primeira']
+  #segunda  = request.json['segunda']
+
+  if primeira and segunda:
+    primeira = float(primeira)
+    segunda  = float(segunda)
+
+    media = (primeira + segunda) / 2
+
+    if media >= 7:
+      resultado = 'Aprovado'
+    elif media >= 4:
+      resultado = 'Recuperação' 
+    else:
+      resultado = 'Reprovado'
+
+  # Cria uma lista
+  #  lista = [
+  #            {'media': media, 'resultado': resultado},
+  #            {'media': media, 'resultado': resultado}
+  #          ]
+  # Retorna uma lista no formato JSON
+  #return jsonify(results = lista)
+
+  # Retorna um resultado em JSON
+  #return jsonify({'resultado': resultado, 'media': media})
+
+  # Retorna resultado por um template html
+  return render_template('index.html', media=media, resultado=resultado) 
+
+
+if __name__ == '__main__':
+    app.run(debug=True) #executa a aplicação
---
+++ print 'hello world'.py
@@ -0,0 +1,7 @@
+print ('hello world')
+
+x = 2
+b = 3
+r = 2 + 3
+
+print (r)
